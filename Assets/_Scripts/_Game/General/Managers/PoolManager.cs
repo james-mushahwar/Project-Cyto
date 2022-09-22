f277453c00c86861b0406a1e92919c73fd669bb3
mu5hjj <james.mushahwar@gmail.com>
2022-09-22 08:20:50 +0100

    smart namespace and singleton class

---
+++ Assets/_Scripts/_Game/General/Managers/PoolManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using _Scripts._Game.General;
+
+//https://www.sebaslab.com/zero-allocation-code-in-unity/
+namespace _Scripts._Game.General.Managers{
+    
+    public abstract class PoolManager<T> : Singleton<PoolManager<T>> where T : MonoBehaviour
+    {
+
+
+        private new void Awake()
+        {
+            base.Awake();
+        }
+    }
+    
+}
---
+++ Assets/_Scripts/_Game/General/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts._Game.General {
+
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static T m_Instance;
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = (T)FindObjectOfType(typeof(T));
+                }
+
+                return m_Instance;
+            }
+        }
+
+        public void Awake()
+        {
+            if (m_Instance != null && m_Instance != this as T)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                m_Instance = this as T;
+            }
+        }
+    }
+}
