7ec4880874ce012c00618f0233e98b54bba657db
mu5hjj <james.mushahwar@gmail.com>
2022-10-28 08:18:03 +0100

    test push sourcetree

--- Assets/_Scripts/_Game/Sequencer/SequencerManager.cs
+++ Assets/_Scripts/_Game/Sequencer/SequencerManager.cs
@@ -18,7 +18,7 @@ namespace _Scripts._Game.Sequencer{
         // Start is called before the first frame update
         void Start()
         {
-            
+            // test
         }
     
         // Update is called once per frame
