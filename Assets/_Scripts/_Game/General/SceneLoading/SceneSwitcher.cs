497295173f18723073811e79038a27f38c40e4fe
mu5hjj <james.mushahwar@gmail.com>
2022-11-07 08:33:12 +0000

    Async scene switching first pass. Scene switcher prefab

--- Assets/_Scripts/_Game/General/SceneLoading/SceneSwitcher.cs
+++ Assets/_Scripts/_Game/General/SceneLoading/SceneSwitcher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 
 namespace _Scripts._Game.General.SceneLoading{
     
@@ -17,16 +18,89 @@ namespace _Scripts._Game.General.SceneLoading{
         [SerializeField]
         private Collider2D[] _colliders = new Collider2D[2];
         private SceneLoadHandshake _handshake;
+
+        private IEnumerator[] _asyncSceneOperation = new IEnumerator[2];
         // Start is called before the first frame update
         void Start()
         {
-            
+            for (int i = 0; i < 2; ++i)
+            {
+                SceneSwitcherCollider ssc = _colliders[i].gameObject.GetComponent<SceneSwitcherCollider>();
+                if (ssc)
+                {
+                    ssc.Init(this, i);
+                }
+            }
         }
     
         // Update is called once per frame
         void Update()
         {
             
+        }
+
+        // used to load scene on player exited collider event
+        public void OnPlayerEntered(int index)
+        {
+            Debug.Log("Scene asset name is " + _sceneAssets[index].name);
+            Scene targetScene = SceneManager.GetSceneByName(_sceneAssets[index].name);
+
+            if (targetScene != null)
+            {
+                if (targetScene.isLoaded || _asyncSceneOperation[index] != null)
+                {
+                    Debug.LogWarning(SceneManager.GetSceneByName(_sceneAssets[index].name) + " is already loaded");
+                    return;
+                }
+            }
+
+            _asyncSceneOperation[index] = LoadSceneAsync(index, _sceneAssets[index].name);
+            StartCoroutine(_asyncSceneOperation[index]);
+        }
+
+        // used to unload scene on on player exited collider event
+        public void OnPlayerExited(int index)
+        {
+            Debug.Log("Scene asset name is " + _sceneAssets[index].name);
+            Scene targetScene = SceneManager.GetSceneByName(_sceneAssets[index].name);
+
+            if (targetScene != null)
+            {
+                if (!targetScene.isLoaded || _asyncSceneOperation[index] != null)
+                {
+                    Debug.LogWarning(SceneManager.GetSceneByName(_sceneAssets[index].name) + " is already unloaded");
+                    return;
+                }
+            }
+
+            _asyncSceneOperation[index] = UnloadSceneAsync(index, _sceneAssets[index].name);
+            StartCoroutine(_asyncSceneOperation[index]);
+        }
+
+        private IEnumerator LoadSceneAsync(int index, string sceneName)
+        {
+            Debug.Log("Trying to load scene: " + sceneName);
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+            while (!operation.isDone)
+            {
+                yield return null;
+            }
+
+            _asyncSceneOperation[index] = null;
+        }
+
+        private IEnumerator UnloadSceneAsync(int index, string sceneName)
+        {
+            Debug.Log("Trying to unload scene: " + sceneName);
+            AsyncOperation operation = SceneManager.UnloadSceneAsync(sceneName);
+
+            while (!operation.isDone)
+            {
+                yield return null;
+            }
+
+            _asyncSceneOperation[index] = null;
         }
     }
     
---
+++ Assets/_Scripts/_Game/General/SceneLoading/SceneSwitcherCollider.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Scripts._Game.General.SceneLoading{
+
+    [RequireComponent(typeof(Collider2D))]
+    public class SceneSwitcherCollider : MonoBehaviour
+    {
+        private SceneSwitcher _sceneSwitcherRef;
+        private Collider2D _collider;
+        private int _colliderIndex;
+
+        [SerializeField]
+        private UnityEvent<int> _collisionEnterEvent;
+        [SerializeField]
+        private UnityEvent<int> _collisionExitEvent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        public void Init(SceneSwitcher ss, int index)
+        {
+            _sceneSwitcherRef = ss;
+            _collider = GetComponent<Collider2D>();
+            _colliderIndex = index;
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            PlayerMovementStateMachine playerMovement = collision.gameObject.GetComponent<PlayerMovementStateMachine>();
+            if (playerMovement)
+            {
+                float dotProduct = Vector3.Dot(playerMovement.Rb.velocity.normalized, _collider.transform.right);
+                if (dotProduct > 0)
+                {
+                    _collisionEnterEvent.Invoke(_colliderIndex);
+                }
+                else
+                {
+                    _collisionExitEvent.Invoke(_colliderIndex);
+                }
+                
+            }
+        }
+
+        private void Update()
+        {
+            //if (_justLeft)
+            //{
+            //    _debugTimer -= Time.deltaTime;
+
+            //    if (_debugTimer < 0)
+            //    {
+            //        _justLeft = false;
+            //    }
+
+            //    ContactPoint2D[] contacts = new ContactPoint2D[1];
+            //    _cachedCollider2D.GetContacts(contacts);
+            //    DrawArrow.ForPointsDebug(new Vector3(transform.position.x, transform.position.y, 0.0f), new Vector3(transform.position.x, transform.position.y + _cameraHeight, 0.0f));
+            //}
+        }
+    }
+    
+}
