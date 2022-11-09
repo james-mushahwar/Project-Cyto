f94f9ab311ec80e7e46e0d8efa6f6f5dfe2032e8
mu5hjj <james.mushahwar@gmail.com>
2022-11-09 08:27:22 +0000

    TMP pro, dialogue canvas setup and typewriter effect script

---
+++ Assets/_Scripts/_Game/UI/Dialogue/TypewiterEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using TMPro;
+using UnityEngine;
+
+namespace _Scripts._Game.UI.Dialogue{
+    
+    public class TypewiterEffect : MonoBehaviour
+    {
+        public void Run(string textToType, TMP_Text textLabel)
+        {
+            StartCoroutine(TypeText(textToType, textLabel));
+        }
+
+        private IEnumerator TypeText(string textToType, TMP_Text textLabel)
+        {
+            float t = 0;
+            int charIndex = 0;
+
+            while (charIndex < textToType.Length)
+            {
+                t += Time.deltaTime;
+                charIndex = Mathf.FloorToInt(t);
+                charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+                textLabel.text = textToType.Substring(0, charIndex);
+
+                yield return null;
+            }
+        }
+    }
+}
