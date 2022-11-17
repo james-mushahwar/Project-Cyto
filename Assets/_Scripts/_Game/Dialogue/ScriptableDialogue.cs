05c0963301411ce0f458d991f77e437e897f200f
mu5hjj <james.mushahwar@gmail.com>
2022-11-17 08:35:45 +0000

    updated scriptable dialogue and removed scriptable phrases

--- Assets/_Scripts/_Game/Dialogue/ScriptableDialogue.cs
+++ Assets/_Scripts/_Game/Dialogue/ScriptableDialogue.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,15 +11,29 @@ namespace _Scripts._Game.Dialogue{
         CharacterOverview,
         CharacterWorld,
         World
-    }  
+    }
+
+    [Serializable]
+    public class Phrase
+    {
+        [SerializeField] private string _phrase;
+        [SerializeField] private bool _receiveInput; // can this phrase receive input
+        [SerializeField] private bool _isSkipable; // can we skip to end of phrase with input
+        [SerializeField] private bool _isAuto;      // does text move onto next automatically?
+        [Header("Text speed properties")]
+        [SerializeField] private float _textSpeed = 1.0f;
+        [SerializeField] private float _fasterTextSpeed = 1.5f;
+        [Header("Wait properties")]
+        [SerializeField] private float _startOfPhraseWait = 0.25f;
+        [SerializeField] private float _endOfPhraseWait = 1.0f;
+    }
 
     [CreateAssetMenu(menuName ="Dialogue/Dialogue")]
     public class ScriptableDialogue : ScriptableObject
     {
         [SerializeField] private EDialogueType _dialogueType;
 
-        [SerializeField] private ScriptablePhrase[] _dialogue;
-    
+        [SerializeField] private Phrase[] _phrases;
     }
     
 }
