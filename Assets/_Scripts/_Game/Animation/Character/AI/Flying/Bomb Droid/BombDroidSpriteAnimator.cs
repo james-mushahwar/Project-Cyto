fe8c73caec7fd355a356ef78cc32bfabae470d97
James Mushahwar <james.mushahwar@gmail.com>
2023-01-10 08:02:14 +0000

    sprite animator for bomb droid with idle and patrol state switching

---
+++ Assets/_Scripts/_Game/Animation/Character/AI/Flying/Bomb Droid/BombDroidSpriteAnimator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using _Scripts._Game.AI.MovementStateMachine.Flying.Bombdroid;
+using _Scripts._Game.AI.MovementStateMachine;
+
+namespace _Scripts._Game.Animation.Character.AI.Flying.BombDroid{
+    
+    public class BombDroidSpriteAnimator : SpriteAnimator
+    {
+        BombDroidAIMovementStateMachine _ctx;
+
+        #region Hashed States
+        public static readonly int Idle = Animator.StringToHash("BombDroidAnim_Idle");
+        public static readonly int Patrol = Animator.StringToHash("BombDroidAnim_Patrol");
+        #endregion
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _ctx = GetComponentInParent<BombDroidAIMovementStateMachine>();
+        }
+
+        private void FixedUpdate()
+        {            
+            if (_ctx.Rb.velocity.x != 0)
+            {
+                Renderer.flipX = _ctx.Rb.velocity.x < 0;
+            } 
+
+            int state = GetState();
+            if (state == CurrentState)
+            {
+                return;
+            }
+
+            Anim.CrossFade(state, 0, 0);
+        }
+
+        protected override int GetState()
+        {
+            BaseAIMovementState currentMovementState = _ctx.CurrentState;
+            if (currentMovementState is BombDroidIdleAIMovementState)
+            {
+                return Idle;
+            }
+            else if (currentMovementState is BombDroidPatrolAIMovementState)
+            {
+                return Patrol;
+            }
+            else if (currentMovementState is BombDroidChaseAIMovementState)
+            {
+                return Idle;
+            }
+            return Idle;
+
+            int LockState(int s, float t) 
+            {
+                //_lockedTill = Time.time + t;
+                return s;
+            }
+        }
+    }
+    
+}
---
+++ Assets/_Scripts/_Game/Animation/SpriteAnimator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts._Game.Animation{
+    
+    public abstract class SpriteAnimator : MonoBehaviour
+    {
+        #region Components
+        private Animator _anim;
+
+        protected Animator Anim { get => _anim; }
+        #endregion
+
+        #region States
+        private int _currentState;
+
+        protected int CurrentState { get => _currentState; set => _currentState = value; }
+
+        protected abstract int GetState();
+        #endregion
+
+        #region Renderer
+        private SpriteRenderer _renderer;
+
+        public SpriteRenderer Renderer { get => _renderer; }
+        #endregion
+
+
+        protected virtual void Awake()
+        {
+            _anim = GetComponent<Animator>();
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+    }
+    
+}
