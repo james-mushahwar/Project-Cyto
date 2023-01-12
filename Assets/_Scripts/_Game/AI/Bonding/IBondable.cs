189cc8a59bfa24bf381a4c5c9175e842b2baf1c2
James Mushahwar <james.mushahwar@gmail.com>
2023-01-12 07:56:41 +0000

    Bonding interfaces WIP

---
+++ Assets/_Scripts/_Game/AI/Bonding/IBondable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts._Game.AI.Bonding{
+    
+    public interface IBondable
+    {
+        void OnMovementInput(InputAction.CallbackContext context); // left stick
+        void OnDirectionInput(InputAction.CallbackContext context); // right stick
+        void OnJumpInput(InputAction.CallbackContext context); // A button
+        void OnDashInput(InputAction.CallbackContext context); // RB button
+        void OnFloatInput(InputAction.CallbackContext context); // RT trigger
+        void OnBounceInput(InputAction.CallbackContext context); // LT trigger
+    }
+    
+}
