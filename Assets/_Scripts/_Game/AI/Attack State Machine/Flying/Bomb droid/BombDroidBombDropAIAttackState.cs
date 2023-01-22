e090a61229562589e6130eae83fe5d322972d763
mu5hjj <james.mushahwar@gmail.com>
2023-01-22 21:38:49 +0000

    Bomb drop attack state timer and exit state transitions

--- Assets/_Scripts/_Game/AI/Attack State Machine/Flying/Bomb droid/BombDroidBombDropAIAttackState.cs
+++ Assets/_Scripts/_Game/AI/Attack State Machine/Flying/Bomb droid/BombDroidBombDropAIAttackState.cs
@@ -6,18 +6,29 @@ namespace _Scripts._Game.AI.AttackStateMachine.Flying.Bombdroid{
     
     public class BombDroidBombDropAIAttackState : BaseAIAttackState
     {
+        private BombDroidAIAttackStateMachine _bdCtx;
+        private bool _hasDroppedBomb;
+
         public BombDroidBombDropAIAttackState(AIAttackStateMachineBase ctx, AIAttackStateMachineFactory factory) : base(ctx, factory)
         {
+            _bdCtx = ctx as BombDroidAIAttackStateMachine;
         }
 
         public override bool CheckSwitchStates()
         {
+            if (_stateTimer >= _bdCtx.BombDropStateDuration)
+            {
+                SwitchStates(_factory.GetState(AIAttackState.Idle));
+                return true;
+            }
             return false;
         }
 
         public override void EnterState()
         {
             Debug.Log("BOOOOM Bomb drop!");
+            _stateTimer = 0.0f;
+            _hasDroppedBomb = false;
         }
 
         public override void ExitState()
@@ -32,7 +43,14 @@ namespace _Scripts._Game.AI.AttackStateMachine.Flying.Bombdroid{
 
         public override void ManagedStateTick()
         {
-            
+            _stateTimer += Time.deltaTime;
+            if (CheckSwitchStates() == false)
+            {
+                if (_stateTimer >= _bdCtx.BombDropBuildUpDuration && !_hasDroppedBomb)
+                {
+                    // drop bomb
+                }
+            }
         }
     }
     
