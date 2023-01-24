c5684f2fe7ba4f0500f81f4d2442da425b5edc56
mu5hjj <james.mushahwar@gmail.com>
2023-01-24 07:42:58 +0000

    Created template script and .cs.txt templates for AI attack and movement classes

---
+++ Assets/_Scripts/Editor tools/Templates/EditorTemplateClasses.cs
@@ -0,0 +1,52 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace _Scripts.Editortools.Templates{
+    
+    public class EditorTemplateClasses
+    {
+        #region AI
+        private const string _aiPath = "Assets/_Scripts/Editor tools/Templates/AI/";
+
+        private const string _aiMovementState = "AIMovementState.cs.txt";
+
+        [MenuItem(itemName: "Assets/Create/Scripts/AI/AI Movement State", isValidateFunction: false, priority: 51)]
+        public static void CreateAIMovementStateFromTemplate()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(_aiPath + _aiMovementState, "AIMovementState.cs");
+        }
+        
+        private const string _aiBondedMovementState = "AIBondedMovementState.cs.txt";
+
+        [MenuItem(itemName: "Assets/Create/Scripts/AI/AI Bonded Movement State", isValidateFunction: false, priority: 51)]
+        public static void CreateAIBondedMovementStateFromTemplate()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(_aiPath + _aiBondedMovementState, "AIBondedMovementState.cs");
+        }
+
+        private const string _aiAttackState = "AIAttackState.cs.txt";
+
+        [MenuItem(itemName: "Assets/Create/Scripts/AI/AI Attack State", isValidateFunction: false, priority: 51)]
+        public static void CreateAIAttackStateFromTemplate()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(_aiPath + _aiAttackState, "AIAttackState.cs");
+        }
+        #endregion
+
+
+        #region General
+        private const string _generalPath = "Assets/_Scripts/Editor tools/Templates/General/";
+        private const string _emptyClassTemplate = "EmptyClass.cs.txt";
+
+        [MenuItem(itemName: "Assets/Create/Scripts/General/C# Class", isValidateFunction: false, priority: 51)]
+        public static void CreateClassFromTemplate()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(_generalPath + _emptyClassTemplate, "EmptyClass.cs");
+        }
+        #endregion
+
+    }
+
+}
+#endif
