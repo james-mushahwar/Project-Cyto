eca67303da625b963c5b468f07afc07d0be16fdd
James Mushahwar <james.mushahwar@gmail.com>
2023-03-15 09:03:50 +0000

    Mushroom archer new generated scripts

---
+++ Assets/_Scripts/_Game/AI/Attack State Machine/Ground/Mushroom Archer/MushroomArcherIdleAIAttackState.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using _Scripts._Game.AI.AttackStateMachine;
+
+namespace _Scripts._Game.AI.AttackStateMachine.Ground.MushroomArcher{
+    
+    public class MushroomArcherIdleAIAttackState : BaseAIAttackState
+    {
+        public MushroomArcherIdleAIAttackState(AIAttackStateMachineBase ctx, AIAttackStateMachineFactory factory) : base(ctx, factory)
+        {
+    
+        }
+    
+        public override bool CheckSwitchStates()
+        {
+            return false;
+        }
+    
+        public override void EnterState()
+        {
+            
+        }
+    
+        public override void ExitState()
+        {
+            
+        }
+    
+        public override void InitialiseState()
+        {
+            
+        }
+    
+        public override void ManagedStateTick()
+        {
+            
+        }
+    }
+    
+}
