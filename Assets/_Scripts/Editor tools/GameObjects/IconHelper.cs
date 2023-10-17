b0734f1ab04fbc526a0cfa8adec77696ea9e5128
mu5hjj <james.mushahwar@gmail.com>
2023-10-17 09:51:02 +0100

    Icons added to prefabs. Restructured waypoint and WP prefab. Icon helper and manager for auto icon assignment for spawn points WIP.

---
+++ Assets/_Scripts/Editor tools/GameObjects/IconHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Scripts._Game.General.Spawning.AI;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace _Scripts.Editortools.GameObjects{
+    
+    [ExecuteInEditMode]
+    public class IconHelper : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+        #if UNITY_EDITOR
+            UpdateIcon();
+        #endif
+        }
+
+        void UpdateIcon()
+        {
+            Component[] comps = GetComponentsInParent(typeof(MonoBehaviour));
+            Debug.Log("ON GUI");
+            foreach (Component comp in comps)
+            {
+                SpawnPoint sp = comp as SpawnPoint;
+                if (sp != null)
+                {
+                    IconManager.SetIcon(gameObject, IconManager.Icon.DiamondOrange);
+                }
+            }
+        }
+    }
+    
+}
---
+++ Assets/_Scripts/Editor tools/GameObjects/IconManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.Rendering.VolumeComponent;
+
+namespace _Scripts.Editortools.GameObjects
+{
+
+    public class IconManager
+    {
+
+        public enum LabelIcon
+        {
+            Gray = 0,
+            Blue,
+            Teal,
+            Green,
+            Yellow,
+            Orange,
+            Red,
+            Purple
+        }
+
+        public enum Icon
+        {
+            CircleGray = 0,
+            CircleBlue,
+            CircleTeal,
+            CircleGreen,
+            CircleYellow,
+            CircleOrange,
+            CircleRed,
+            CirclePurple,
+            DiamondGray,
+            DiamondBlue,
+            DiamondTeal,
+            DiamondGreen,
+            DiamondYellow,
+            DiamondOrange,
+            DiamondRed,
+            DiamondPurple
+        }
+
+        private static GUIContent[] labelIcons;
+        private static GUIContent[] largeIcons;
+
+        public static void SetIcon(GameObject gObj, LabelIcon icon)
+        {
+            if (labelIcons == null)
+            {
+                labelIcons = GetTextures("sv_label_", string.Empty, 0, 8);
+            }
+
+            SetIcon(gObj, labelIcons[(int)icon].image as Texture2D);
+        }
+
+        public static void SetIcon(GameObject gObj, Icon icon)
+        {
+            if (largeIcons == null)
+            {
+                largeIcons = GetTextures("sv_icon_dot", "_pix16_gizmo", 0, 16);
+            }
+
+            SetIcon(gObj, largeIcons[(int)icon].image as Texture2D);
+        }
+
+        private static void SetIcon(GameObject gObj, Texture2D texture)
+        {
+            var ty = typeof(EditorGUIUtility);
+            EditorGUIUtility.SetIconForObject(gObj, texture);
+            //var mi = ty.GetMethod("SetIconForObject", BindingFlags.NonPublic | BindingFlags.Static);
+            //mi.MakeGenericMethod(typeof(UnityEngine.Object));
+            //mi.Invoke(null, new object[] { gObj, texture });
+        }
+
+        private static GUIContent[] GetTextures(string baseName, string postFix, int startIndex, int count)
+        {
+            GUIContent[] guiContentArray = new GUIContent[count];
+
+            var t = typeof(EditorGUIUtility);
+            //var mi = t.GetMethod("IconContent", BindingFlags.NonPublic | BindingFlags.Static, null, new Type[] { typeof(string) }, null);
+            //mi.MakeGenericMethod(typeof(UnityEngine.Object));
+            for (int index = 0; index < count; ++index)
+            {
+                guiContentArray[index] = EditorGUIUtility.IconContent(baseName + (object)(startIndex + index) + postFix);
+                //guiContentArray[index] = mi.Invoke(null, new object[] { baseName + (object)(startIndex + index) + postFix }) as GUIContent;
+            }
+
+            return guiContentArray;
+        }
+    }
+
+}
