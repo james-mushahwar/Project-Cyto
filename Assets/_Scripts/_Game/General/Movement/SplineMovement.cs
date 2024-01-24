8255d46908ebba0ed4cd79d9f951cc3011767122
mu5hjj <james.mushahwar@gmail.com>
2024-01-24 23:51:08 +0000

    challenge room 1_2 outline and splines imported

---
+++ Assets/_Scripts/_Game/General/Movement/SplineMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace _Scripts._Game.General.Movement{
+    
+    public class SplineMovement : MonoBehaviour
+    {
+        [SerializeField] 
+        private GameObject _moveGO;
+        [SerializeField] 
+        private SplineContainer _spline; 
+
+        private void Awake()
+        {
+            if (_moveGO == null)
+            {
+                _moveGO = gameObject;
+            }
+        }
+    }
+    
+}
