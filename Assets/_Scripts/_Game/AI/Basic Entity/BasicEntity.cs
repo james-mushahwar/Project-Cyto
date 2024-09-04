4a89d2ddf8470f2159d6b9356b2e0b8a52140335
James Mushahwar <james.mushahwar@gmail.com>
2024-09-04 17:04:48 +0100

    basic behaviour wip

---
+++ Assets/_Scripts/_Game/AI/Basic Entity/BasicEntity.cs
@@ -0,0 +1,34 @@
+﻿using _Scripts._Game.General;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+namespace _Scripts._Game.AI.BasicEntity{
+
+    public abstract class BasicEntity : MonoBehaviour, IDamageable
+    {
+        public virtual IExposable Exposable  { get => throw new System.NotImplementedException(); }
+
+        public virtual Vector2 DamageDirection { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public virtual List<EDamageType> DamageTypesToIgnore => throw new System.NotImplementedException();
+
+        public virtual List<EDamageType> DamageTypesToAccept => throw new System.NotImplementedException();
+
+        public virtual Transform Transform => throw new System.NotImplementedException();
+
+        public virtual EEntityType EntityType => throw new System.NotImplementedException();
+
+        public virtual bool IsAlive()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual bool TakeDamage(EDamageType damageType, EEntityType causer, Vector3 damagePosition)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
